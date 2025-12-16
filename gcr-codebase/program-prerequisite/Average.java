43b8717ea208fb38e186be52d0f4f97e5dc1d2fe
Ananya Shukla <ananyashukla990@gmail.com>
2025-12-16 22:58:08 +0530

    [Ananya] : Added a program of Average of three numbers

---
+++ gcr-codebase/program-prerequisite/Average.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Average{
+	public static void main(String[]args){
+		Scanner sc=new Scanner(System.in);
+		int x=sc.nextInt();
+		int y=sc.nextInt();
+		int z=sc.nextInt();
+		int ans=(x+y+z)/3;
+		System.out.println(ans);
+	}
+}
