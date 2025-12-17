73b895ae639bfc9293e045ee95f6525b2048508a
Ananya Shukla <ananyashukla990@gmail.com>
2025-12-17 21:24:49 +0530

    [Ananya] : Added program of Conersion of kilometers to miles

---
+++ gcr-codebase/programming-elements/level-one/ConersionOfKilometersToMiles.java
@@ -0,0 +1,16 @@
+//Conersion of kilometers to miles
+
+public class ConersionOfKilometersToMiles
+{
+	public static void main(String []args)
+	{
+		//Taking input details
+		int kilometers=10.8;
+		
+        //Calculating miles
+        double miles= kilometers/1.6;
+		
+		//Printing miles
+        System.out.println(miles);
+    }
+}
