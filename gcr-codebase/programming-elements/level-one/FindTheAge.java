393708c0428b46bad799f64970e10be47c4e130c
Ananya Shukla <ananyashukla990@gmail.com>
2025-12-17 21:28:42 +0530

    [Ananya] : Added program of Finding the age

---
+++ gcr-codebase/programming-elements/level-one/FindTheAge.java
@@ -0,0 +1,15 @@
+//Finding Harry's age in 2024
+public class FindTheAge{
+	public static void main(String[]args)
+	{   
+	    //Giving input details
+		int birthYear=2000;
+		int currentYear=2024;
+		
+		//Age calculation
+		int presentAge=currentYear-birthYear;
+		
+		//Printing the age
+		System.out.println("Harry's age in 2024 is : " + presentAge);
+	}
+}
