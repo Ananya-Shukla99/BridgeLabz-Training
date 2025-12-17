21b80a852addc8af8ec6691f80ac9710332fd070
Ananya Shukla <ananyashukla990@gmail.com>
2025-12-17 21:23:25 +0530

    [Ananya] : Added program of calculator

---
+++ gcr-codebase/programming-elements/level-one/Calculator.java
@@ -0,0 +1,23 @@
+// a basic calculator program
+import java.util.Scanner;
+public class Calculator{
+	public static void main(String []args)
+	{
+		//Taking input
+	    Scanner s=new Scanner(System.in);
+		System.out.println("Num1 :");
+		float num1=s.nextInt();
+		System.out.println("Num2 :");
+		float num2 =s.nextInt();
+		
+		//Calculation
+		float add=num1+num2;
+		float sub=num1-num2;
+		float mul=num1*num2;
+		float division=(num1>num2)?num1/num2:num2/num1;
+		
+		//Printing discount
+		System.out.println( The addition, subtraction, multiplication and division value of 2 numbers "+num1+" and "+num2+" is "+add+","+sub+","+mul+", and "+division);
+);
+	}
+}
