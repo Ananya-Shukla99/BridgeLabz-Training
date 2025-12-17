55188378da9b411f20c3c3245d5d5eb20879e3f7
Ananya Shukla <ananyashukla990@gmail.com>
2025-12-17 23:34:07 +0530

    [Ananya] : Added a program for calculating side of square

---
+++ gcr-codebase/programming-elements/level-two/CalculatingSideOfSquare.java
@@ -0,0 +1,18 @@
+//a program to find the side of the square
+import java.util.*;
+public class  CalculatingSideOfSquare
+{
+	public static void main(String []args)
+	{
+		//Taking input details
+		Scanner sc=new Scanner(System.in);
+        int perimeter=sc.nextInt();
+		
+        //Calculating ares
+        int side=perimeter/4;
+		
+		//Printing area
+        System.out.println("The length of the side is "+side+" whose perimeter is "+perimeter);
+        sc.close();
+    }
+}
