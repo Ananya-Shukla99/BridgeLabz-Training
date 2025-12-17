ed974d533b3fd32a4fcb75b035821c1b07216e01
Ananya Shukla <ananyashukla990@gmail.com>
2025-12-17 23:38:32 +0530

    [Ananya] : Added a program for calculating the distance in yards and miles

---
+++ gcr-codebase/programming-elements/level-two/FindingTheDistanceInYardsAndMiles.java
@@ -0,0 +1,21 @@
+// a program the find the distance in yards and miles
+import java.util.*;
+public class FindingTheDistanceInYardsAndMiles
+{
+	public static void main(String []args)
+	{
+		//Taking input details
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Input foot :");
+        double feets=sc.nextDouble();
+		
+        //Calculating conversion
+		double yards=feets/3;
+		double miles=yards/1760;
+        
+		
+		//Printing area
+        System.out.println(" Your Distance in feet "+feets+" while in yards is "+yards+" and miles is "+miles);
+		sc.close();
+    }
+}
