dca4478d75ecf30175503292c4d2930a9a8412a0
Ananya Shukla <ananyashukla990@gmail.com>
2025-12-19 00:15:50 +0530

    [Ananya] : Added a program of Printing odd or even

---
+++ gcr-codebase/control-flow/Level-two/PrintingOddOrEven.java
@@ -0,0 +1,24 @@
+//A Program to find odd and even numbers between 1 to the number entered by the user.
+
+import java.util.Scanner;
+public class PrintingOddOrEven
+{
+	public static void main(String []args)
+	{
+		// getting number from the user
+		Scanner input=new Scanner(System.in);
+        System.out.println("Enter number :");
+        int number = input.nextInt();
+		
+		//printing odd and even numbers between 1 to the number from the user input 
+		for(int i=1; i<=number; i++){
+		    if(i%2==0){
+				System.out.println(i+" is a even number");
+			}
+			else{
+				System.out.println(i+" is a odd number");
+			}
+		}
+		input.close();
+	}
+}
