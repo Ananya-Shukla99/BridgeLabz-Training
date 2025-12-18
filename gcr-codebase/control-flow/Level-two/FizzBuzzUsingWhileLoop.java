7a1abd6a73834940675ba5c517af83f2e0cd619b
Ananya Shukla <ananyashukla990@gmail.com>
2025-12-19 00:05:26 +0530

    [Ananya] : Added a program of fizz Buzz using while loop

---
+++ gcr-codebase/control-flow/Level-two/FizzBuzzUsingWhileLoop.java
@@ -0,0 +1,31 @@
+//A Program to check for a positive integer , if positive integer, loop and print the number, but for multiples of 3 print "Fizz"
+// instead of the number, for multiples of 5 print "Buzz", and for multiples of both print "FizzBuzz".
+import java.util.Scanner;
+public class FizzBuzzUsingWhileLoop
+{
+	public static void main(String []args)
+	{
+		// getting number from the user
+		Scanner input=new Scanner(System.in);
+        System.out.println("Enter number :");
+        int number = input.nextInt();
+		
+		//printing positive numbers between 1 to the number from the user input checking there divisibility 
+		while(number>0){
+		    if(i%3==0){
+				System.out.println("Fizz");
+			}
+			else if (i%5==0){
+				System.out.println("Buzz");
+			}
+			else if(i%5==0 && i%3==0){
+				System.out.println("FizzBuzz");
+			}
+			else{
+				System.out.println(i);
+			}
+		}
+		number--;
+		input.close();
+	}
+}
