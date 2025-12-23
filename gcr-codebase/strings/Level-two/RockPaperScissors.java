22a4fd26291ec73dcfdaa680e68671c32f28cf4e
Ananya Shukla <ananyashukla990@gmail.com>
2025-12-23 12:50:57 +0530

    [Ananya] : Added a program of rock paper scissors

---
+++ gcr-codebase/strings/Level-two/RockPaperScissors.java
@@ -0,0 +1,112 @@
+//Rock-Paper-Scissors is a game played between a minimum of two players
+import java.util.Scanner;
+
+public class RockPaperScissors {
+
+    // method to get computer choice using Math.random()
+    public static String getComputerChoice()
+	{
+        int choice = (int) (Math.random() * 3);
+        if (choice == 0){
+            return "rock";}
+        else if (choice == 1){
+            return "paper";}
+        else{
+            return "scissors";}
+    }
+
+    // method to find winner
+    public static String findWinner(String user, String computer) 
+	{
+        if (user.equals(computer)) {
+            return "Draw";
+        }
+
+        if ((user.equals("rock") && computer.equals("scissors")) ||
+            (user.equals("paper") && computer.equals("rock")) ||
+            (user.equals("scissors") && computer.equals("paper"))) {
+            return "User";
+        }
+
+        return "Computer";
+    }
+
+    // method to calculate average and percentage
+    public static String[][] calculateStats(int userWins, int computerWins, int totalGames) 
+	{
+        String[][] stats = new String[2][3];
+
+        double userPercent = ((double) userWins / totalGames) * 100;
+        double compPercent = ((double) computerWins / totalGames) * 100;
+
+        stats[0][0] = "User";
+        stats[0][1] = String.valueOf(userWins);
+        stats[0][2] = String.format("%.2f", userPercent);
+
+        stats[1][0] = "Computer";
+        stats[1][1] = String.valueOf(computerWins);
+        stats[1][2] = String.format("%.2f", compPercent);
+
+        return stats;
+    }
+
+    // Method to display results
+    public static void displayResults(String[][] games, String[][] stats) {
+
+        System.out.println("\nGame Results:");
+        System.out.println("Game\tUser\tComputer\tWinner");
+        System.out.println();
+
+        for (int i = 0; i < games.length; i++) {
+            System.out.println((i + 1) + "\t" + games[i][0] + "\t" + games[i][1] + "\t\t" + games[i][2]);
+        }
+
+        System.out.println("\nOverall Statistics:");
+        System.out.println("Player\tWins\tWin Percentage");
+        System.out.println();
+
+        for (int i = 0; i < stats.length; i++) {
+            System.out.println(stats[i][0] + "\t" + stats[i][1] + "\t" + stats[i][2] + "%");
+        }
+    }
+
+    public static void main(String[] args) 
+	{
+	
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of games: ");
+        int gamesCount = sc.nextInt();
+        sc.nextLine(); 
+
+        String[][] gameResults = new String[gamesCount][3];
+        int userWins = 0;
+        int computerWins = 0;
+
+        for (int i = 0; i < gamesCount; i++) 
+		{
+            System.out.print("\nGame " + (i + 1) + " - Enter your choice (rock/paper/scissors): ");
+            String userChoice = sc.nextLine().toLowerCase();
+
+            String computerChoice = getComputerChoice();
+            String winner = findWinner(userChoice, computerChoice);
+
+            if (winner.equals("User"))
+            {
+      			userWins++;
+				}
+            else if (winner.equals("Computer")){
+                computerWins++;
+			}
+
+            gameResults[i][0] = userChoice;
+            gameResults[i][1] = computerChoice;
+            gameResults[i][2] = winner;
+        }
+
+        String[][] stats = calculateStats(userWins, computerWins, gamesCount);
+        displayResults(gameResults, stats);
+
+        sc.close();
+    }
+}
