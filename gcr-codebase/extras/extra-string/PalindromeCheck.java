97331435a52121efef337665882497cc8e5b160a
Ananya Shukla <ananyashukla990@gmail.com>
2025-12-24 10:26:38 +0530

    [Ananya] : Added a program to check if a string is palindrome or not

---
+++ gcr-codebase/extras/extra-string/PalindromeCheck.java
@@ -0,0 +1,42 @@
+// a program to check palindrome
+import java.util.*;
+
+public class PalindromeCheck{
+	
+	public static boolean palindromeCheck(s){
+		
+		int start=0;
+		int end=s.length()-1;
+		while(start<=end){
+		    
+			if(s.charAt(start)!=s.charAt(end)){
+			    return false;
+			}
+		    start ++;
+			end--;
+		}
+		return true;
+	}
+	
+	public static void main(String[]args){
+		
+		Scanner sc=new Scanner(System.in);
+		
+		System.out.println("Enter the string : ");
+		String s= sc.next();
+		
+		int ans=1;
+		//calling the function
+		boolean falg=palindrome(s);
+		
+		if(flag){
+		    System.out.println("String is palindrome");
+		}
+		
+		else{
+		    System.out.println("This is not palindrome");
+		}
+		sc.close();
+	}
+	
+}
