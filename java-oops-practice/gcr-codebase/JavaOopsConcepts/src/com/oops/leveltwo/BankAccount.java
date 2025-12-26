1e3cd8311af692e692490ebe875c6920d30be6af
Ananya Shukla <ananyashukla990@gmail.com>
2025-12-27 01:06:23 +0530

    [Ananya] : Added a Bank Account class

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/oops/leveltwo/BankAccount.java
@@ -0,0 +1,64 @@
+package com.oops.leveltwo;
+
+public class BankAccount {
+    private String accountHolder;
+    private String accountNumber;
+    private double balance;
+    
+    public BankAccount(String accountHolder, String accountNumber, double balance ) {
+    	this.accountHolder=accountHolder;
+    	this.accountNumber=accountNumber;
+    	this.balance=balance;
+    	
+    }
+    public static void displayBank() {
+    	System.out.println("State Bank of India");
+    }
+    public void deposit(double amount) {
+    	if(amount >0) {
+    		balance+=amount;
+    		System.out.println("Deposited : "+balance);
+    	}
+    	else {
+    		System.out.println("amount must be positive");
+    	}
+    }
+    
+    public void withdraw(double amount) {
+    	if (amount <= balance && amount > 0) {
+			balance -= amount;
+			System.out.println("Withdraw amount is : "+amount);
+		}
+		else if (amount <= 0) {
+			System.out.println("Withdrawal amount must be positive");
+		}
+		else {
+			System.out.println("Insufficient balance");
+		}
+
+    }
+    public void displayBalance() {
+        System.out.println("Current balance: " + balance);
+    }
+
+
+    // Main method for testing
+    public static void main(String[] args) {
+        // Creating a BankAccount object
+        BankAccount account = new BankAccount("Lynda", "123456789", 700.00);
+       
+        //Display the Bank name
+        displayBank();
+       
+        // Display initial balance
+        account.displayBalance();
+        // Deposit money
+        account.deposit(200.00);
+        account.displayBalance();
+        // Withdraw money
+        account.withdraw(100.00);
+        account.displayBalance();
+        // Try to withdraw more than the available balance
+        account.withdraw(900.00);
+    }
+}
