361306fa8e0e8b1226183770ffca776e758db1ff
Ananya Shukla <ananyashukla990@gmail.com>
2025-12-31 11:53:21 +0530

    [Ananya] : Added a ECommerce main class

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/designprinciples/levelone/ECommerceMain.java
@@ -0,0 +1,23 @@
+package com.designprinciples.levelone;
+
+public class ECommerceMain {
+    public static void main(String[] args) {
+
+        CustomerOrder customer = new CustomerOrder("Alice");
+
+        Product p1 = new Product("Laptop", 75000);
+        Product p2 = new Product("Mouse", 1500);
+        Product p3 = new Product("Keyboard", 2500);
+
+        Order order1 = new Order(101);
+
+        // Communication
+        order1.addProduct(p1);
+        order1.addProduct(p2);
+        order1.addProduct(p3);
+
+        customer.placeOrder(order1);
+
+        order1.showOrderDetails();
+    }
+}
