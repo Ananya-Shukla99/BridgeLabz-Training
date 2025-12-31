060763702db9a8f072191b15ba3c9ef2691d43b8
Ananya Shukla <ananyashukla990@gmail.com>
2025-12-31 11:55:26 +0530

    [Ananya] : Added a Hospital class

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/designprinciples/levelone/Hospital.java
@@ -0,0 +1,31 @@
+package com.designprinciples.levelone;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Hospital {
+
+    private List<Doctor> doctors;
+    private List<Patient> patients;
+
+    public Hospital() {
+        doctors = new ArrayList<>();
+        patients = new ArrayList<>();
+    }
+
+    public void addDoctor(Doctor doctor) {
+        doctors.add(doctor);
+    }
+
+    public void addPatient(Patient patient) {
+        patients.add(patient);
+    }
+
+    public List<Doctor> getDoctors() {
+        return doctors;
+    }
+
+    public List<Patient> getPatients() {
+        return patients;
+    }
+}
