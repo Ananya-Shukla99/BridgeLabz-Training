effea0ab8d2350fda2224095e48c575f9f434bb9
Ananya Shukla <ananyashukla990@gmail.com>
2026-01-01 14:59:27 +0530

    [Ananya] : Added a program of Savings Account Class

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/Inheritance/leveltwo/SavingsAccount.java
@@ -0,0 +1,15 @@
+package com.Inheritance.leveltwo;
+
+public class SavingsAccount extends BankAccount {
+
+	private double interestRate;
+
+    public SavingsAccount(String accountNumber, double balance, double interestRate) {
+        super(accountNumber, balance);
+        this.interestRate = interestRate;
+    }
+
+    public void displayAccountType() {
+        System.out.println("Account Type: Savings Account");
+    }
+}
