be7eba18555ff984ce6f1f303eeb42b0e3916d25
Ananya Shukla <ananyashukla990@gmail.com>
2026-01-01 15:00:22 +0530

    [Ananya] : Added a program of Shipped Order Class

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/Inheritance/leveltwo/ShippedOrder.java
@@ -0,0 +1,15 @@
+package com.Inheritance.leveltwo;
+
+class ShippedOrder extends Order {
+    protected String trackingNumber;
+
+    public ShippedOrder(String orderId, String orderDate, String trackingNumber) {
+        super(orderId, orderDate);
+        this.trackingNumber = trackingNumber;
+    }
+
+    @Override
+    public String getOrderStatus() {
+        return "Order Shipped";
+    }
+}
