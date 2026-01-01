423063f23882d7def5e0f64714824a97f4cecb4e
Ananya Shukla <ananyashukla990@gmail.com>
2026-01-01 14:33:50 +0530

    [Ananya] : Added a program of BankAccount Class

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/Inheritance/leveltwo/BankAccount.java
@@ -0,0 +1,18 @@
+package com.Inheritance.leveltwo;
+
+public class BankAccount {
+
+	protected String accountNumber;
+    protected double balance;
+
+    public BankAccount(String accountNumber, double balance) {
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+
+    public void displayAccountDetails() {
+        System.out.println("Account Number: " + accountNumber);
+        System.out.println("Balance: $" + balance);
+    }
+    
+}
