7a3c5f5cd11625945b033ed6677cde478bc800e9
Ananya Shukla <ananyashukla990@gmail.com>
2026-01-01 14:42:33 +0530

    [Ananya] : Added a program of Device Main Class

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/Inheritance/leveltwo/DeviceMain.java
@@ -0,0 +1,9 @@
+package com.Inheritance.leveltwo;
+
+public class DeviceMain {
+
+	 public static void main(String[] args) {
+	        Thermostat thermostat = new Thermostat("TH-101", "ON", 24);
+	        thermostat.displayStatus();
+	 }
+}
