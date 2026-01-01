5d069d03b6503a9cd6e578c7d30de1f00bf0c407
Ananya Shukla <ananyashukla990@gmail.com>
2026-01-01 14:45:40 +0530

    [Ananya] : Added a program of FixedDeposit Account Class

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/Inheritance/leveltwo/FixedDepositAccount.java
@@ -0,0 +1,15 @@
+package com.Inheritance.leveltwo;
+
+public class FixedDepositAccount extends BankAccount {
+
+	private int maturityPeriod;
+
+    public FixedDepositAccount(String accountNumber, double balance, int maturityPeriod) {
+        super(accountNumber, balance);
+        this.maturityPeriod = maturityPeriod;
+    }
+
+    public void displayAccountType() {
+        System.out.println("Account Type: Fixed Deposit Account");
+    }
+}
