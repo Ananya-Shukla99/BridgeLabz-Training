5738386fc1b294635a00b5939e013cbc8ab2dc8a
Ananya Shukla <ananyashukla990@gmail.com>
2026-01-01 14:43:14 +0530

    [Ananya] : Added a program of Electric Vehicle Class

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/Inheritance/leveltwo/ElectricVehicle.java
@@ -0,0 +1,13 @@
+package com.Inheritance.leveltwo;
+
+public class ElectricVehicle extends Vehicle
+{
+
+	public ElectricVehicle(int maxSpeed, String model ) {
+		super(maxSpeed, model);
+	}
+	
+	public void charge() {
+        System.out.println("Charging the electric vehicle...");
+    }
+}
