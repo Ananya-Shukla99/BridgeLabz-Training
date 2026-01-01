d23fc38506e9040888271927ba0a8d01bc7085de
Ananya Shukla <ananyashukla990@gmail.com>
2026-01-01 12:53:45 +0530

    [Ananya] : Added a Bird Class

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/Inheritance/levelone/Bird.java
@@ -0,0 +1,12 @@
+package com.Inheritance.levelone;
+
+public class Bird extends Animal {
+	
+	public Bird(String name, int age) {
+		super(name, age);
+	}
+
+	public void makeSound() {
+		System.out.println("Bird Chipper !");
+	}
+}
