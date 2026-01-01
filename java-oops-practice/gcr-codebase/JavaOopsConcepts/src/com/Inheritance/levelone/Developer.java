96449fe96ab205bdfbd5cda936df49d23ffa417b
Ananya Shukla <ananyashukla990@gmail.com>
2026-01-01 12:55:02 +0530

    [Ananya] : Added a Developer Class

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/Inheritance/levelone/Developer.java
@@ -0,0 +1,15 @@
+package com.Inheritance.levelone;
+
+class Developer extends Employee {
+    String programmingLanguage;
+
+    Developer(String name, int id, double salary, String programmingLanguage) {
+        super(name, id, salary);
+        this.programmingLanguage = programmingLanguage;
+    }
+
+    public void displayDetails() {
+        super.displayDetails();
+        System.out.println("Language: " + programmingLanguage);
+    }
+}
