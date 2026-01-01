1cc484b3d3156bdaf6c950930b0554fc87d8d5cc
Ananya Shukla <ananyashukla990@gmail.com>
2026-01-01 14:52:27 +0530

    [Ananya] : Added a program of Person Class

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/Inheritance/leveltwo/Personn.java
@@ -0,0 +1,11 @@
+package com.Inheritance.leveltwo;
+
+class Personn {
+    protected String name;
+    protected int id;
+
+    public Personn(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+}
