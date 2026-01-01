33e364f76057f1595e2b61cbec4c7abe4a2d83a8
Ananya Shukla <ananyashukla990@gmail.com>
2026-01-01 14:47:12 +0530

    [Ananya] : Added a program of Order Class

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/Inheritance/leveltwo/Order.java
@@ -0,0 +1,21 @@
+package com.Inheritance.leveltwo;
+
+public class Order {
+
+	protected String orderId;
+    protected String orderDate;
+
+    public Order(String orderId, String orderDate) {
+        this.orderId = orderId;
+        this.orderDate = orderDate;
+    }
+
+    public String getOrderStatus() {
+        return "Order Placed";
+    }
+    
+    public void displayOrderDetails() {
+        System.out.println("Order ID: " + orderId);
+        System.out.println("Order Date: " + orderDate);
+    }
+}
