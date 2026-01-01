17368d9681d871227f22d6298be7acfca6115866
Ananya Shukla <ananyashukla990@gmail.com>
2026-01-01 12:55:28 +0530

    [Ananya] : Added a Dog Class

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/Inheritance/levelone/Dog.java
@@ -0,0 +1,12 @@
+package com.Inheritance.levelone;
+
+public class Dog extends Animal{
+
+	public Dog (String name, int age){
+		super(name, age);
+	}
+	
+	public void makeSound() {
+		System.out.println("Dog barks !");
+	}
+}
