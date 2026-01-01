aac99e8e77a6c9e9f375074ec4769b0a8d8da2c0
Ananya Shukla <ananyashukla990@gmail.com>
2026-01-01 14:38:14 +0530

    [Ananya] : Added a program of Course Class

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/Inheritance/leveltwo/Course.java
@@ -0,0 +1,17 @@
+package com.Inheritance.leveltwo;
+
+public class Course {
+
+	protected String courseName;
+    protected int duration; 
+
+    public Course(String courseName, int duration) {
+        this.courseName = courseName;
+        this.duration = duration;
+    }
+
+    public void displayCourseInfo() {
+        System.out.println("Course Name: " + courseName);
+        System.out.println("Duration: " + duration + " hours");
+    }
+}
