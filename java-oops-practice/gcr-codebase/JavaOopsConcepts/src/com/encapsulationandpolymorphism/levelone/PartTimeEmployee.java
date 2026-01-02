bea7371d1bcebb733a5c0c488f622801dd63845a
Ananya Shukla <ananyashukla990@gmail.com>
2026-01-02 10:28:35 +0530

    [Ananya] : Added a PartTime Employee class

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/encapsulationandpolymorphism/levelone/PartTimeEmployee.java
@@ -0,0 +1,26 @@
+package com.encapsulationandpolymorphism.levelone;
+
+public class PartTimeEmployee extends Employee implements Department {
+    private int hoursWorked;
+    private String department;
+
+    public PartTimeEmployee(int employeeId, String name, double hourlyRate, int hoursWorked) {
+        super(employeeId, name, hourlyRate);
+        this.hoursWorked = hoursWorked;
+    }
+
+    @Override
+    public double calculateSalary() {
+        return baseSalary * hoursWorked;
+    }
+
+    @Override
+    public void assignDepartment(String departmentName) {
+        this.department = departmentName;
+    }
+
+    @Override
+    public String getDepartmentDetails() {
+        return department;
+    }
+}
