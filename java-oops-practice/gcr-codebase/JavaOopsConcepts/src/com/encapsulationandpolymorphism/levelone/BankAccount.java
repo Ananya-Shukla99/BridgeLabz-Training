e59b300d25bf80675a42c3ae89a47dbe87dae651
Ananya Shukla <ananyashukla990@gmail.com>
2026-01-02 09:42:51 +0530

    [Ananya] : Added a BankAccount class

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/encapsulationandpolymorphism/levelone/BankAccount.java
@@ -0,0 +1,45 @@
+package com.encapsulationandpolymorphism.levelone;
+
+public abstract class BankAccount {
+
+    private String accountNumber;
+    private String holderName;
+    private double balance;
+
+    public BankAccount(String accountNumber, String holderName, double balance) {
+        this.accountNumber = accountNumber;
+        this.holderName = holderName;
+        this.balance = balance;
+    }
+
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+        }
+    }
+
+    public void withdraw(double amount) {
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+        } else {
+            System.out.println("Insufficient balance or invalid amount.");
+        }
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    protected void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public String getAccountSummary() {
+        return "Account Number: " + accountNumber +
+               ", Holder: " + holderName +
+               ", Balance: " + balance;
+    }
+
+    public abstract double calculateInterest();
+}
