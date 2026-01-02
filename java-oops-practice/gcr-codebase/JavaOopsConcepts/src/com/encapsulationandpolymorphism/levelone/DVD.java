b0b1baff92d0f2cf5fbb2ccca6ea6e933780035e
Ananya Shukla <ananyashukla990@gmail.com>
2026-01-02 09:54:48 +0530

    [Ananya] : Added a DVD class

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/encapsulationandpolymorphism/levelone/DVD.java
@@ -0,0 +1,30 @@
+package com.encapsulationandpolymorphism.levelone;
+
+public class DVD extends LibraryItem implements Reservable {
+
+    private boolean available = true;
+
+    public DVD(int itemId, String title, String author) {
+        super(itemId, title, author);
+    }
+
+    @Override
+    public int getLoanDuration() {
+        return 3; 
+    }
+
+    @Override
+    public void reserveItem() {
+        if (available) {
+            available = false;
+            System.out.println("DVD reserved successfully.");
+        } else {
+            System.out.println("DVD is not available.");
+        }
+    }
+
+    @Override
+    public boolean checkAvailability() {
+        return available;
+    }
+}
