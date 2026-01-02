fb677abfda08debda5a12e45fd7533854ea28f1a
Ananya Shukla <ananyashukla990@gmail.com>
2026-01-02 09:55:15 +0530

    [Ananya] : Added a Department class

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/encapsulationandpolymorphism/levelone/Department.java
@@ -0,0 +1,8 @@
+package com.encapsulationandpolymorphism.levelone;
+
+public interface Department {
+	
+    void assignDepartment(String departmentName);
+    
+    String getDepartmentDetails();
+}
