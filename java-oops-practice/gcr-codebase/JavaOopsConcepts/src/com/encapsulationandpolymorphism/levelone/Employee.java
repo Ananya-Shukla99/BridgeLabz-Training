37758b1916215dc61bcfd8147d168a526b9e74f5
Ananya Shukla <ananyashukla990@gmail.com>
2026-01-02 09:57:35 +0530

    [Ananya] : Added a Employee class

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/encapsulationandpolymorphism/levelone/Employee.java
@@ -0,0 +1,43 @@
+package com.encapsulationandpolymorphism.levelone;
+
+//Employee Management System
+
+public abstract class Employee {
+	
+    private int employeeId;
+    private String name;
+    protected double baseSalary;
+
+    public Employee(int employeeId, String name, double baseSalary) {
+        this.employeeId = employeeId;
+        this.name = name;
+        this.baseSalary = baseSalary;
+    }
+
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public abstract double calculateSalary();
+
+    
+    public void displayDetails() {
+        System.out.println("Employee ID: " + employeeId);
+        System.out.println("Name: " + name);
+        System.out.println("Salary: " + calculateSalary());
+    }
+}
