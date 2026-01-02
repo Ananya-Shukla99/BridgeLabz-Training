df73f64913e4c7c380625db025984b7b659db765
Ananya Shukla <ananyashukla990@gmail.com>
2026-01-02 10:36:47 +0530

    [Ananya] : Added a Vehicle class

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/encapsulationandpolymorphism/levelone/Vehicle.java
@@ -0,0 +1,24 @@
+package com.encapsulationandpolymorphism.levelone;
+
+public abstract class Vehicle {
+    private String vehicleNumber;
+    private String type;
+    protected double rentalRate;
+
+    public Vehicle(String vehicleNumber, String type, double rentalRate) {
+        this.vehicleNumber = vehicleNumber;
+        this.type = type;
+        this.rentalRate = rentalRate;
+    }
+
+    public String getVehicleNumber() {
+        return vehicleNumber;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public abstract double calculateRentalCost(int days);
+}
+
