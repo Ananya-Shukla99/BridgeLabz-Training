46b8744b9191f08c8131c18ea8edaad33240198b
Ananya Shukla <ananyashukla990@gmail.com>
2026-01-02 10:08:19 +0530

    [Ananya] : Added a Full Time Employee class

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/encapsulationandpolymorphism/levelone/FullTimeEmployee.java
@@ -0,0 +1,25 @@
+package com.encapsulationandpolymorphism.levelone;
+
+public class FullTimeEmployee extends Employee implements Department {
+    private String department;
+
+    public FullTimeEmployee(int employeeId, String name, double baseSalary) {
+        super(employeeId, name, baseSalary);
+    }
+
+    @Override
+    public double calculateSalary() {
+        return baseSalary;
+    }
+
+    @Override
+    public void assignDepartment(String departmentName) {
+        this.department = departmentName;
+    }
+
+    @Override
+    public String getDepartmentDetails() {
+        return department;
+    }
+}
+
