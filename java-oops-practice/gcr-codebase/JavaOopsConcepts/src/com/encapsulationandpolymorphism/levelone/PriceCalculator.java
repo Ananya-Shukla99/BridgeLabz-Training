fdb15723b8cac2ee9d4a394d5b5097e74ee2ddeb
Ananya Shukla <ananyashukla990@gmail.com>
2026-01-02 10:29:46 +0530

    [Ananya] : Added a Price Calculator class

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/encapsulationandpolymorphism/levelone/PriceCalculator.java
@@ -0,0 +1,26 @@
+package com.encapsulationandpolymorphism.levelone;
+
+import java.util.List;
+
+public class PriceCalculator {
+
+    public static void printFinalPrices(List<Product> products) {
+        for (Product product : products) {
+            double tax = 0;
+
+            if (product instanceof Taxable) {
+                tax = ((Taxable) product).calculateTax();
+            }
+
+            double discount = product.calculateDiscount();
+            double finalPrice = product.getPrice() + tax - discount;
+
+            System.out.println("Product: " + product.getName());
+            System.out.println("Base Price: " + product.getPrice());
+            System.out.println("Discount: " + discount);
+            System.out.println("Tax: " + tax);
+            System.out.println("Final Price: " + finalPrice);
+            System.out.println("---------------------------");
+        }
+    }
+}
