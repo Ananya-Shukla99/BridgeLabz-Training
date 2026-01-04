f99f38c1cbf4065efd1e2f5c699988f67da083ac
Ananya Shukla <ananyashukla990@gmail.com>
2026-01-04 21:06:35 +0530

    [Ananya] : Added a program University course enrollment system

--- java-oops-practice/scenario-based-codebase/OopsScenarioBased/src/com/universitycourseenrollmentsystem/Enrollment.java
+++ java-oops-practice/scenario-based-codebase/OopsScenarioBased/src/com/universitycourseenrollmentsystem/Enrollment.java
@@ -2,21 +2,27 @@ package com.universitycourseenrollmentsystem;
 
 class Enrollment {
 	
+	// Private fields - data encapsulation
     private Student student;
     private Course course;
 
+    //constructor
     Enrollment(Student student, Course course) {
         this.student = student;
         this.course = course;
     }
 
+    //getter for student 
     public Student getStudent() {
         return student;
     }
-
+    
+    //method for course
     public Course getCourse() {
         return course;
     }
+    
+    //method to display info 
     public void show() {
     	System.out.println("Student " + student +"course "+course);
     }
