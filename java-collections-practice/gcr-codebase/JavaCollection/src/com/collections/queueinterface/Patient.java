016257084bb068b19424cd19c1fb648f7a08bf22
Ananya Shukla <ananyashukla990@gmail.com>
2026-01-24 23:30:33 +0530

    [Ananya] : Added Patient model for hospital triage system

---
+++ java-collections-practice/gcr-codebase/JavaCollection/src/com/collections/queueinterface/Patient.java
@@ -0,0 +1,15 @@
+package com.collections.queueinterface;
+
+//For hospital management system
+class Patient {
+
+	//attributes
+	String name;
+	int severity;
+
+	//constructor 
+	public Patient(String name, int severity) {
+		this.name = name;
+		this.severity = severity;
+	}
+}
