5639b033c5acb7d68111fd40258bd39a3b97e43a
Ananya Shukla <ananyashukla990@gmail.com>
2026-01-26 02:13:51 +0530

    [Ananya] : Added a program of Bank Transaction System with Checked and Custom Exception

---
+++ java-collections-practice/gcr-codebase/JavaCollection/src/com/exceptions/BankTransactionSystem.java
@@ -0,0 +1,36 @@
+package com.exceptions;
+
+import java.util.Scanner;
+
+public class BankTransactionSystem {
+
+	public static void withdraw(int amount, double balance)throws InsufficientBalanceException, IllegalArgumentException {
+		if(amount>balance) {
+			throw new  InsufficientBalanceException("Insufficient balance!");
+		}
+		if(amount<=0) {
+			throw new  IllegalArgumentException("Invalid amount!");
+		}
+		else {
+			System.out.println("Withdrawal successful, new balance: "+ (balance-amount));
+		}
+	}
+	public static void main(String[]args) {
+		Scanner sc= new Scanner (System.in);
+		
+		final double balance=5000.0;
+		try {
+			System.out.println("Enter withdrawal amount :");
+			int amount= sc.nextInt();
+		    
+			withdraw(amount, balance);
+		}
+		catch(InsufficientBalanceException | IllegalArgumentException ex){
+			System.out.println(ex.getMessage());
+		}finally {
+			sc.close();
+		}
+		
+	}
+	
+}
