aa6a712e4a4f60219602b662a6deed6e9d780def
Ananya Shukla <ananyashukla990@gmail.com>
2026-01-25 19:51:40 +0530

    [Ananya] : Added basic file handling operations program

---
+++ java-collections-practice/gcr-codebase/JavaCollection/src/com/javastreams/FileHandling.java
@@ -0,0 +1,28 @@
+package com.javastreams;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class FileHandling {
+    public static void main(String[] args) {
+
+        String sourceFile = "source.txt";
+        String destinationFile = "destination.txt";
+
+        try (FileInputStream fis = new FileInputStream(sourceFile);
+             FileOutputStream fos = new FileOutputStream(destinationFile)) {
+
+            int data;
+            while ((data = fis.read()) != -1) {
+                fos.write(data);
+            }
+
+            System.out.println("File copied successfully.");
+
+        } 
+        catch (IOException e) {
+            System.out.println("Source file not found or error while reading the file.");
+        }
+    }
+}
