6ffe425a1f7283da8111bc3652ff82cc415ac3f0
Ananya Shukla <ananyashukla990@gmail.com>
2026-01-26 13:08:33 +0530

    [Ananya] : Added a program of Bank Transaction System with coustom use of exceptions

--- java-collections-practice/gcr-codebase/JavaCollection/src/com/exceptions/BankTransactionSystem.java
+++ java-collections-practice/gcr-codebase/JavaCollection/src/com/exceptions/BankTransactionSystem.java
@@ -4,33 +4,33 @@ import java.util.Scanner;
 
 public class BankTransactionSystem {
 
-	public static void withdraw(int amount, double balance)throws InsufficientBalanceException, IllegalArgumentException {
-		if(amount>balance) {
-			throw new  InsufficientBalanceException("Insufficient balance!");
+	public static void withdraw(int amount, double balance)
+			throws InsufficientBalanceException, IllegalArgumentException {
+		if (amount > balance) {
+			throw new InsufficientBalanceException("Insufficient balance!");
 		}
-		if(amount<=0) {
-			throw new  IllegalArgumentException("Invalid amount!");
-		}
-		else {
-			System.out.println("Withdrawal successful, new balance: "+ (balance-amount));
+		if (amount <= 0) {
+			throw new IllegalArgumentException("Invalid amount!");
+		} else {
+			System.out.println("Withdrawal successful, new balance: " + (balance - amount));
 		}
 	}
-	public static void main(String[]args) {
-		Scanner sc= new Scanner (System.in);
-		
-		final double balance=5000.0;
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		final double balance = 5000.0;
 		try {
 			System.out.println("Enter withdrawal amount :");
-			int amount= sc.nextInt();
-		    
+			int amount = sc.nextInt();
+
 			withdraw(amount, balance);
-		}
-		catch(InsufficientBalanceException | IllegalArgumentException ex){
+		} catch (InsufficientBalanceException | IllegalArgumentException ex) {
 			System.out.println(ex.getMessage());
-		}finally {
+		} finally {
 			sc.close();
 		}
-		
+
 	}
-	
+
 }
