0ba17393cde44b10dbb4f9e815cfdd3fe16a5e56
Ananya Shukla <ananyashukla990@gmail.com>
2026-01-26 16:45:56 +0530

    [Ananya] : Added a program of Banking Transactions for JUnit tests to check correct balance updates

---
+++ java-collections-practice/gcr-codebase/JavaCollection/src/com/testing/BankingTransactions.java
@@ -0,0 +1,28 @@
+package com.testing;
+
+public class BankingTransactions {
+
+	 private double balance;
+
+	    public BankingTransactions (double initialBalance) {
+	        this.balance = initialBalance;
+	    }
+
+	    public void deposit(double amount) {
+	        if (amount > 0) {
+	            balance += amount;
+	        }
+	    }
+
+	    public boolean withdraw(double amount) {
+	        if (amount > 0 && amount <= balance) {
+	            balance -= amount;
+	            return true;
+	        }
+	        return false; 
+	    }
+
+	    public double getBalance() {
+	        return balance;
+	    }
+}
