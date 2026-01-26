62cbd13936e8f1162a49b1d712fb60920ef3c71d
Ananya Shukla <ananyashukla990@gmail.com>
2026-01-26 20:13:22 +0530

    [Ananya] : Implemented reflection logic to invoke private methods dynamically

---
+++ java-collections-practice/gcr-codebase/JavaCollection/src/com/reflection/invokeprivatemethod/Calculator.java
@@ -0,0 +1,9 @@
+package com.reflection.invokeprivatemethod;
+
+public class Calculator {
+
+    @SuppressWarnings("unused")
+	private int multiply(int a, int b) {
+        return a * b;
+    }
+}
---
+++ java-collections-practice/gcr-codebase/JavaCollection/src/com/reflection/invokeprivatemethod/InvokePrivateMethod.java
@@ -0,0 +1,25 @@
+package com.reflection.invokeprivatemethod;
+
+import java.lang.reflect.Method;
+
+public class InvokePrivateMethod {
+
+    public static void main(String[] args) {
+        try {
+            Calculator calculator = new Calculator();
+
+            Class<?> cls = calculator.getClass();
+
+            Method method = cls.getDeclaredMethod("multiply", int.class, int.class);
+
+            method.setAccessible(true);
+
+            int result = (int) method.invoke(calculator, 5, 4);
+
+            System.out.println("Result of multiply: " + result);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
