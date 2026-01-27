b52843dbb361adc344a0b6da5bbaea929b0b1912
Ananya Shukla <ananyashukla990@gmail.com>
2026-01-27 20:47:15 +0530

    [Ananya] : Added Student POJO for JSON read and write operations

---
+++ java-iostreams-practice/gcr-codebase/JavaIoStreams/src/com/jsondata/Student.java
@@ -0,0 +1,48 @@
+package com.jsondata;
+
+import java.util.List;
+
+//class Student 
+public class Student {
+	private String name;
+	private int age;
+	private List<String> subjects;
+
+	public Student() {
+	}
+
+	public Student(String name, int age, List<String> subjects) {
+		this.name = name;
+		this.age = age;
+		this.subjects = subjects;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public List<String> getSubjects() {
+		return subjects;
+	}
+
+	public void setSubjects(List<String> subjects) {
+		this.subjects = subjects;
+	}
+
+	@Override
+	public String toString() {
+		return "Student{name='" + name + "', age=" + age + ", subjects=" + subjects + "}";
+	}
+}
