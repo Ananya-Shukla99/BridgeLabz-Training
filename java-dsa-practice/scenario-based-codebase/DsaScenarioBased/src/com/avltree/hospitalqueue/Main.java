128e35fe16bb4f419fa65af5bfd73e649a6550ef
Ananya Shukla <ananyashukla990@gmail.com>
2026-01-27 20:23:02 +0530

    [Ananya] : Implemented a program of Hospital Queue Management using AVL Tree

--- java-dsa-practice/scenario-based-codebase/DsaScenarioBased/src/com/avltree/hospitalqueue/Main.java
+++ java-dsa-practice/scenario-based-codebase/DsaScenarioBased/src/com/avltree/hospitalqueue/Main.java
@@ -1,5 +1,23 @@
 package com.avltree.hospitalqueue;
 
 public class Main {
+	public static void main(String[] args) {
 
+		Patient queue = new Patient();
+
+		queue.root = queue.insert(queue.root, "Alice", 900);
+		queue.insert(queue.root, "Bob", 930);
+		queue.insert(queue.root, "Charlie", 915);
+		queue.insert(queue.root, "David", 945);
+
+		System.out.println("Patients by Arrival Time:");
+		queue.display(queue.root);
+
+		System.out.println("\nDischarging patient checked in at 915");
+		queue.root = queue.delete(queue.root, 915);
+
+		System.out.println("\nUpdated Queue:");
+		queue.display(queue.root);
+		
+	}
 }
