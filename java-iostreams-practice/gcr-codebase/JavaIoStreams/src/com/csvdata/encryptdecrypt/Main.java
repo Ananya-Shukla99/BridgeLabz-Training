f216c98cab57ccb50a0ac2eaba706be02f830321
Ananya Shukla <ananyashukla990@gmail.com>
2026-02-02 09:52:39 +0530

    Updated java8-programming-practice

--- java-iostreams-practice/gcr-codebase/JavaIoStreams/src/com/csvdata/encryptdecrypt/Main.java
+++
@@ -1,8 +0,0 @@
-package com.csvdata.encryptdecrypt;
-
-public class Main {
-	public static void main(String[] args) throws Exception {
-		WriteEncryptedCSV.write("src/com/csvdata/employees.csv");
-		ReadDecryptedCSV.read("src/com/csvdata/employees.csv");
-	}
-}
